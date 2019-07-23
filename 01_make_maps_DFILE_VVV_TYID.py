a03568b17ca0adb672148d46ebdb574a7005db4b
Hiroyuki Tomita <tomita@MacPro3.local>
2019-07-23 10:29:38 +0900

    the first commit

---
+++ 01_make_maps_DFILE_VVV_TYID.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+#
+# 01_make_maps_DFILE_VVV_TYID
+
+import sys
+import subprocess
+import pandas as pd
+
+nargv = len(sys.argv)
+if nargv == 4:
+  dfile=sys.argv[1]
+  var=sys.argv[2]
+  tyid=sys.argv[3]
+else:
+  print("input error")
+
+print(tyid)
+
+# get best track data
+command='./Best_Track/best_track.py DATA:time,lon,lat,p,ws ' +  tyid + ' > ty1013.txt'
+subprocess.getoutput(command)
+
+# read date,clon,clat
+df = pd.read_table('./ty1013.txt', header=None, sep=' ',skipinitialspace=True, skiprows=[0],names=['time','lon','lat','p','wind'])
+print(df)
+
+# define date_time as a new column (For ferret time format)
+df['date_time'] = pd.to_datetime(df['time'], format='%y%m%d%H')
+df['date_time_ferret'] = df['date_time'].dt.strftime("%d-%b-%Y %H:%M:%S")
+
+# cal longitude and latitude
+df['longitude'] = df['lon']/10
+df['latitude'] = df['lat']/10
+
+# print Best track data for TYID
+target=['date_time_ferret','longitude','latitude']
+print(df[target])
+
+# Loop and make figures
+n=0
+for date in df['date_time_ferret']:
+ print (n,date,df.iloc[n]['longitude'],df.iloc[n]['latitude'])
+ longitude = df.iloc[n]['longitude']
+ latitude = df.iloc[n]['latitude']
+ task='./make_jnl.csh ' + dfile + ' ' + var + ' ' + str(n).zfill(3) + ' ' + date + ' ' + str(longitude) + ' ' + str(latitude)
+ print(task)
+ res=subprocess.getoutput(task)
+ print(res)
+ command='ferret -nojnl -gif < ferret_go.jnl'
+ res=subprocess.getoutput(command)
+ print(res)
+ n += 1
+ 
+
+
+
+
+
---
+++ make_jnl.csh
@@ -0,0 +1,18 @@
+#!/bin/csh
+set dfile=$argv[1]
+set var=$argv[2]
+set n=$argv[3]
+set date=$argv[4]
+set time=$argv[5]
+set lon=$argv[6]
+set lat=$argv[7]
+set output=ferret_go.jnl
+
+set lev="(0,400,40)"
+set tyid="1013"
+
+echo "use $dfile" > $output
+echo "go tymap.jnl $date $time $lon $lat $var $lev ty${tyid}_$n " >> $output
+echo "quit" >> $output
+
+
